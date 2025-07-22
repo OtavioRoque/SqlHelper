7f65ea408972cfc27c13311c0a869f8ea5e5e1ae
otavi <otavi@DESKTOP-LTHBJR6>
2025-07-22 11:03:21 -0300

    Add project files.

---
+++ UserControls/RadioButtonsOpcoesImportacao.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace SqlHelper.UserControls
+{
+    public partial class RadioButtonsOpcoesImportacao : UserControl
+    {
+        public RadioButtonsOpcoesImportacao()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ UserControls/TextboxComBotaoLimpar.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows.Controls;
+
+namespace SqlHelper.UserControls
+{
+    public partial class TextboxComBotaoLimpar : UserControl
+    {
+        public TextboxComBotaoLimpar()
+        {
+            DataContext = this;
+            InitializeComponent();
+        }
+
+        private string _placeHolder;
+
+        public string PlaceHolder
+        {
+            get { return _placeHolder; }
+            set { _placeHolder = value; }
+        }
+
+        private void btnLimpar_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            txtInput.Clear();
+            txtInput.Focus();
+        }
+    }
+}
