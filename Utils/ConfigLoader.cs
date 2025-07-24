ea2460e2b6850a02e150c2ba31bf0a5dac1d508b
Otávio Roque <roqueotavio5@gmail.com>
2025-07-24 16:20:17 -0300

    criei classes de acesso ao banco de dados na nova pasta utils

---
+++ Utils/ConfigLoader.cs
@@ -0,0 +1,25 @@
+﻿using System.Configuration;
+using System.IO;
+
+namespace SqlHelper.Utils
+{
+    public static class ConfigLoader
+    {
+        public static string? ObterConnectionString(string nome)
+        {
+            string localConfigPath = "App.Local.config";
+
+            if (File.Exists(localConfigPath))
+            {
+                var map = new ExeConfigurationFileMap { ExeConfigFilename = localConfigPath };
+                var config = ConfigurationManager.OpenMappedExeConfiguration(map, ConfigurationUserLevel.None);
+                var connectionString = config.ConnectionStrings.ConnectionStrings[nome];
+
+                if (connectionString != null)
+                    return connectionString.ConnectionString;
+            }
+
+            return ConfigurationManager.ConnectionStrings[nome]?.ConnectionString;
+        }
+    }
+}
---
+++ Utils/DatabaseAccess.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using Microsoft.Data.SqlClient;
+
+#pragma warning disable CS8601
+#pragma warning disable CS8603
+
+namespace SqlHelper.Utils
+{
+    public static class DatabaseAccess
+    {
+        private static readonly string connectionString = ConfigLoader.ObterConnectionString("ConexaoLocal");
+
+        public static DataTable GetDataTable(string sql)
+        {
+            try
+            {
+                using var conn = new SqlConnection(connectionString);
+                using var cmd = new SqlCommand(sql, conn);
+                using var adapter = new SqlDataAdapter(cmd);
+
+                var tabela = new DataTable();
+                adapter.Fill(tabela);
+                return tabela;
+            }
+            catch (Exception ex)
+            {
+                throw LancarExcecaoComandoSql(sql, ex);
+            }
+        }
+
+        public static string GetScalar(string sql)
+        {
+            try
+            {
+                using var conn = new SqlConnection(connectionString);
+                using var cmd = new SqlCommand(sql, conn);
+
+                conn.Open();
+                return cmd.ExecuteScalar().ToString();
+            }
+            catch (Exception ex)
+            {
+                throw LancarExcecaoComandoSql(sql, ex);
+            }
+        }
+
+        public static int Execute(string sql)
+        {
+            try
+            {
+                using var conn = new SqlConnection(connectionString);
+                using var cmd = new SqlCommand(sql, conn);
+
+                conn.Open();
+                return cmd.ExecuteNonQuery();
+
+            }
+            catch (Exception ex)
+            {
+                throw LancarExcecaoComandoSql(sql, ex);
+            }
+        }
+
+        private static Exception LancarExcecaoComandoSql(string comandoSql, Exception exception)
+        {
+            string mensagem = $"Erro ao executar comando SQL: {comandoSql}. Detalhes: {exception.Message}";
+            return new Exception(mensagem, exception);
+        }
+    }
+}
