d6da8bd52ccdbd2319c7bff77ce8c1650a271fe9
Otávio Roque <roqueotavio5@gmail.com>
2025-08-19 17:53:47 -0300

    ajustes as propriedades e os metodos de adicionar coluna

--- Models/TableModel.cs
+++ Models/TableModel.cs
@@ -1,23 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SqlHelper.Models
+﻿namespace SqlHelper.Models
 {
     public class TableModel
     {
-        public bool IsChecked { get; set; } = false;
+        public string Schema { get; }
         public string Name { get; }
-        public long RowCount { get; }
-        public List<ColumnModel> Columns { get; }
+        public long RowCount { get; set; }
+        public bool IsChecked { get; set; }
+        public IReadOnlyList<ColumnModel> Columns => _columns;
 
-        public TableModel(string name, long rowCount, List<ColumnModel> columns)
+        private List<ColumnModel> _columns = new List<ColumnModel>();
+
+        public TableModel(string schema, string name)
         {
+            Schema = schema;
             Name = name;
-            RowCount = rowCount;
-            Columns = columns ?? new List<ColumnModel>();
+        }
+
+        public void AddColumn(ColumnModel column)
+        {
+            if (ColumnExists(column.Name))
+                return;
+
+            _columns.Add(column);
+        }
+
+        private bool ColumnExists(string columnName)
+        {
+            return _columns.Any(t => t.Name.Equals(columnName, StringComparison.OrdinalIgnoreCase));
         }
     }
 }
