1df90ffff5245f2870a7e2bb86d707627bb34bf2
Otávio Roque <roqueotavio5@gmail.com>
2025-08-26 08:19:30 -0300

    Removi a lógica do code behind da MainWindow para em seguida colocar no MainWindowViewModel.cs

--- Views/Windows/MainWindow.xaml.cs
+++ Views/Windows/MainWindow.xaml.cs
@@ -1,8 +1,5 @@
-﻿using System.Data;
-using System.Windows;
-using System.Windows.Controls;
-
-#pragma warning disable CS8600
+﻿using System.Windows;
+using SqlHelper.ViewModels;
 
 namespace SqlHelper.Views.Windows
 {
@@ -11,57 +8,7 @@ namespace SqlHelper.Views.Windows
         public MainWindow()
         {
             InitializeComponent();
-            DataContext = this;
-            this.Loaded += MainWindow_Loaded;
+            DataContext = new MainWindowViewModel();
         }
-
-        #region Events
-
-        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
-        {
-            LoadCbDatabases();
-        }
-
-        private void cbDatabases_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            if (!TryGetSelectedDatabase(out string databaseName))
-                return;
-
-            string sql = $@"
-                SELECT t.name AS Tabela, SUM(p.rows) AS Rows
-                FROM {databaseName}.sys.tables t
-                JOIN {databaseName}.sys.partitions p ON t.object_id = p.object_id
-                WHERE p.index_id IN (0,1) AND Rows > 0
-                GROUP BY t.name";
-
-            dgTables.ItemsSource = DB.FillDataTable(sql).DefaultView;
-        }
-
-        #endregion
-
-        #region Private Methods
-
-        private void LoadCbDatabases()
-        {
-            string sql = "SELECT name FROM sys.databases ORDER BY name";
-            DataTable dt = DB.FillDataTable(sql);
-
-            cbDatabases.ItemsSource = dt.DefaultView;
-            cbDatabases.DisplayMemberPath = "name";
-            cbDatabases.SelectedValuePath = "name";
-        }
-
-        private string GetSelectedDatabase()
-        {
-            return cbDatabases.SelectedValue.ToString() ?? string.Empty;
-        }
-
-        private bool TryGetSelectedDatabase(out string databaseName)
-        {
-            databaseName = GetSelectedDatabase();
-            return !string.IsNullOrEmpty(databaseName);
-        }
-
-        #endregion
     }
 }
