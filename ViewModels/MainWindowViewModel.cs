e36b66eb76cb3f12a21a60b4e5c29bba600d3d21
Otávio Roque <roqueotavio5@gmail.com>
2025-08-27 17:35:00 -0300

    Fiz o Binding do cbDatabases do MainWindow.xaml no MainWindowViewModel.cs

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,19 +1,56 @@
-﻿using System.ComponentModel;
+﻿using SqlHelper.Models;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Data;
+using System.Runtime.CompilerServices;
 
 namespace SqlHelper.ViewModels
 {
     public class MainWindowViewModel : INotifyPropertyChanged
     {
+        private DatabaseModel? _selectedDatabase;
+
+        public DatabaseModel? SelectedDatabase
+        {
+            get => _selectedDatabase;
+            set
+            {
+                if (_selectedDatabase != value)
+                {
+                    _selectedDatabase = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public ObservableCollection<DatabaseModel> Databases { get; } = new ObservableCollection<DatabaseModel>();
+
         public MainWindowViewModel()
         {
-            OnLoaded();
+            LoadDatabases();
         }
 
         public event PropertyChangedEventHandler? PropertyChanged;
 
-        private void OnLoaded()
+        private void OnPropertyChanged([CallerMemberName] string propertyName = "")
         {
-            // aqui quero carregar os dados iniciais
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void LoadDatabases()
+        {
+            string sql = "SELECT name FROM sys.databases";
+            var dtDatabases = DB.FillDataTable(sql);
+
+            foreach (DataRow row in dtDatabases.Rows)
+            {
+                string databaseName = row["name"].ToString() ?? string.Empty;
+
+                if (string.IsNullOrWhiteSpace(databaseName))
+                    continue;
+
+                Databases.Add(new DatabaseModel(databaseName));
+            }
         }
     }
 }
