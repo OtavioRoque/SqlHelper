8d42840c348cfcb0a185118927718c28029fe817
Otávio <roqueotavio5@gmail.com>
2025-08-31 21:42:59 -0300

    Adicionei ToSqlDbType() para converter o tipo de columa lido no INFORMATION_SCHEMA pra SqlDbType

--- Utils/ParseHelper.cs
+++ Utils/ParseHelper.cs
@@ -1,4 +1,6 @@
-﻿namespace SqlHelper.Utils
+﻿using System.Data;
+
+namespace SqlHelper.Utils
 {
     public static class ParseHelper
     {
@@ -13,5 +15,42 @@
             long.TryParse(s, out long result);
             return result;
         }
+
+        public static SqlDbType ToSqlDbType(string sqlType)
+        {
+            return sqlType.ToLower() switch
+            {
+                "int" => SqlDbType.Int,
+                "bigint" => SqlDbType.BigInt,
+                "smallint" => SqlDbType.SmallInt,
+                "tinyint" => SqlDbType.TinyInt,
+                "bit" => SqlDbType.Bit,
+                "decimal" => SqlDbType.Decimal,
+                "numeric" => SqlDbType.Decimal,
+                "money" => SqlDbType.Money,
+                "smallmoney" => SqlDbType.SmallMoney,
+                "float" => SqlDbType.Float,
+                "real" => SqlDbType.Real,
+                "date" => SqlDbType.Date,
+                "datetime" => SqlDbType.DateTime,
+                "datetime2" => SqlDbType.DateTime2,
+                "smalldatetime" => SqlDbType.SmallDateTime,
+                "datetimeoffset" => SqlDbType.DateTimeOffset,
+                "time" => SqlDbType.Time,
+                "char" => SqlDbType.Char,
+                "varchar" => SqlDbType.VarChar,
+                "text" => SqlDbType.Text,
+                "nchar" => SqlDbType.NChar,
+                "nvarchar" => SqlDbType.NVarChar,
+                "ntext" => SqlDbType.NText,
+                "binary" => SqlDbType.Binary,
+                "varbinary" => SqlDbType.VarBinary,
+                "image" => SqlDbType.Image,
+                "uniqueidentifier" => SqlDbType.UniqueIdentifier,
+                "xml" => SqlDbType.Xml,
+                _ => SqlDbType.Variant
+            };
+        }
+
     }
 }
