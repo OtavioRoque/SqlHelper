76d349ecff442d676156536e837721fa3ddec323
Otavio <roqueotavio5@gmail.com>
2025-10-08 09:14:26 -0300

    Adicionei uma propriedade do tipo DatabaseModel em TableModel

--- Models/TableModel.cs
+++ Models/TableModel.cs
@@ -4,12 +4,14 @@ namespace SqlHelper.Models
 {
     public class TableModel : ObservableObject
     {
+        public DatabaseModel Database { get; }
         public string Schema { get; }
         public string Name { get; }
         public long RowCount { get; }
 
-        public TableModel(string schema, string name, long rowCount = 0)
+        public TableModel(DatabaseModel database, string schema, string name, long rowCount = 0)
         {
+            Database = database;
             Schema = schema;
             Name = name;
             RowCount = rowCount;
