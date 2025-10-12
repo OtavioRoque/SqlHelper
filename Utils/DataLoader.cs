1ba11aa0bae1dd6ea72a5ac8fafd9ad98be6d1ca
Otávio <roqueotavio5@gmail.com>
2025-10-12 11:41:41 -0300

    Fiz a classe LoadTableData pra carregar os dados das tabelas

---
+++ Utils/DataLoader.cs
@@ -0,0 +1,19 @@
+﻿using SqlHelper.Models;
+using System.Data;
+
+namespace SqlHelper.Utils
+{
+    public static class DataLoader
+    {
+        public static DataTable LoadTableData(DatabaseModel database, TableModel table, int maxRows = 300)
+        {
+            if (database == null || table == null)
+                return new DataTable();
+            if (string.IsNullOrWhiteSpace(database.Name) || string.IsNullOrWhiteSpace(table.Schema) || string.IsNullOrWhiteSpace(table.Name))
+                return new DataTable();
+
+            string sql = $"SELECT TOP {maxRows} * FROM [{database.Name}].[{table.Schema}].[{table.Name}];";
+            return SQL.FillDataTable(sql);
+        }
+    }
+}
