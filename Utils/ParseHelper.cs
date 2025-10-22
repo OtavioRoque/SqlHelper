1c8d5633b66f7851798b4f282aad577263f3f477
Otavio <roqueotavio5@gmail.com>
2025-10-22 18:17:52 -0300

    Adicionei comentários XML para os métodos de ParseHelper

--- Utils/ParseHelper.cs
+++ Utils/ParseHelper.cs
@@ -2,20 +2,40 @@
 
 namespace SqlHelper.Utils
 {
+    /// <summary>
+    /// Contém métodos utilitários para conversão de tipos.
+    /// </summary>
     public static class ParseHelper
     {
+        /// <summary>
+        /// Converte uma string em um int.
+        /// </summary>
+        /// <param name="s">A string a ser convertida.</param>
+        /// <returns>O valor inteiro convertido ou 0 em caso de falha.</returns>
         public static int ToInt(string s)
         {
             _ = int.TryParse(s, out int result);
             return result;
         }
 
+        /// <summary>
+        /// Converte uma string em um long.
+        /// </summary>
+        /// <param name="s">A string a ser convertida.</param>
+        /// <returns>O valor long convertido ou 0 em caso de falha.</returns>
         public static long ToLong(string s)
         {
             _ = long.TryParse(s, out long result);
             return result;
         }
 
+        /// <summary>
+        /// Converte uma string representando um tipo SQL para o correspondente SqlDbType.
+        /// </summary>
+        /// <remarks>
+        /// É recomendado que o parâmetro sqlType seja a informação lida de um INFORMATION_SCHEMA.COLUMNS.DATA_TYPE.
+        /// </remarks>
+        /// <param name="sqlType">O tipo da coluna.</param>
         public static SqlDbType ToSqlDbType(string sqlType)
         {
             return sqlType.ToLower() switch
@@ -51,6 +71,5 @@ namespace SqlHelper.Utils
                 _ => SqlDbType.Variant
             };
         }
-
     }
 }
