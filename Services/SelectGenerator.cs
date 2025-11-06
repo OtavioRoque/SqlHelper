243076ee144c33cc9a02b11bd2a60ac77ce654c5
Otavio <roqueotavio5@gmail.com>
2025-11-06 18:27:34 -0300

    Iniciei o desenvolvimento da classe SelectGenerator

---
+++ Services/SelectGenerator.cs
@@ -0,0 +1,30 @@
+﻿using SqlHelper.Models;
+
+namespace SqlHelper.Services
+{
+    public class SelectGenerator
+    {
+        public enum SelectType
+        {
+            Default,
+            Cliente,
+            Item
+        }
+
+        private readonly DatabaseModel _database;
+        private readonly TableModel _table;
+
+        public SelectGenerator(DatabaseModel database, TableModel table)
+        {
+            _database = database;
+            _table = table;
+        }
+
+        public string GenerateSelect(SelectType selectType)
+        {
+            var columns = MetadataLoader.LoadColumns(_database, _table);
+
+            return string.Empty;
+        }
+    }
+}
